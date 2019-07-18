bbc49b73afae4c22998bfd1b40594ecd522a3935
Kelsey <kmiller202011@gmail.com>
2019-07-17 19:48:52 -0500

    First project. Ran maven, created a project and uploaded to git!

---
+++ src/main/java/com/example/ProjectTrackFirstDay2/ProjectTrackFirstDay2Application.java
@@ -0,0 +1,13 @@
+package com.example.ProjectTrackFirstDay2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTrackFirstDay2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTrackFirstDay2Application.class, args);
+	}
+
+}
